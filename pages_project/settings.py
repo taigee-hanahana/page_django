11991eb4b121cfdbb41ad6051dbed01462507323
Taichi Miyagi <taigee.0516@gmail.com>
2018-08-28 14:21:50 +0900

    New updates for Heroku deployment

--- pages_project/settings.py
+++ pages_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rp7f6jrmo06qr)e25+pxwb7jp^+(t5al8hh4^gz(u5okls=k+%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
